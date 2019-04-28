d687fd08f898644f277aaa6a68921e99e762b050
Sharif Ahmed <sharifducse@gmail.com>
2019-04-28 11:28:54 +0600

    Implemented HATEOAS concepts

--- src/main/java/com/spring/boot/starter/app/rest/controller/UserController.java
+++ src/main/java/com/spring/boot/starter/app/rest/controller/UserController.java
@@ -5,6 +5,8 @@ import com.spring.boot.starter.app.rest.model.User;
 import com.spring.boot.starter.app.rest.repository.UserRepository;
 import com.spring.boot.starter.app.rest.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -14,6 +16,7 @@ import java.net.URI;
 import java.util.List;
 
 import static java.util.Objects.isNull;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
 /**
  * @author sharif.ahmed
@@ -40,14 +43,19 @@ public class UserController {
     }
 
     @GetMapping("/repoUser/{id}")
-    public User getRepoUser(@PathVariable long id) {
+    public Resource<User> getRepoUser(@PathVariable long id) {
         User user = userRepository.findOne(id);
 
         if (isNull(user)) {
             throw new UserNotFoundException("User not found with id = " + id);
         }
 
-        return user;
+        Resource<User> resource = new Resource<>(user);
+
+        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).getUsers());
+        resource.add(linkTo.withRel("allUsers"));
+
+        return resource;
     }
 
     @GetMapping("/users")
